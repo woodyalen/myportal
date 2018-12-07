c1867be7674272cd69c09057a225be956eef4f12
idleo <idleo@yandex.ru>
2018-12-07 12:24:24 +0300

    Added new template for bitrix:crm.entity.editor

--- local/php_interface/include/functions.php
+++ local/php_interface/include/functions.php
@@ -1,13 +1,21 @@
 <?
 
-function dump($obj, $str = "DUMP", $print = false, $dbg = true)
+/**
+ * Dump for debug
+ *
+ * @param        $obj
+ * @param string $str
+ * @param bool   $print
+ * @param bool   $dbg
+ */
+function dump( $obj, $str = "DUMP", $print = false, $dbg = true)
 {
-    $debug = array_shift(debug_backtrace());
-    
     $str = " >>>>> " . $str . " <<<<< ";
-    if ($dbg)
-        $str .= " file: " . $debug['file'] . "  //  line: " . $debug['line'];
-
+    if ($dbg) {
+        $debug = array_shift( debug_backtrace() );
+        $str .= " file: " . $debug[ 'file' ] . "  //  line: " . $debug[ 'line' ];
+    }
+    
     if($print)
         echo $str, "<pre>", print_r($obj), "</pre>";
     else
